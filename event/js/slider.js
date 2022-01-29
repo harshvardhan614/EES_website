6342921415e488231e603e4ea714945dc9bc25aa
Harshvardhan <rharshvardhan614@gmail.com>
2022-01-29 11:07:42 +0530

    done all final changes

--- event/js/slider.js
+++ event/js/slider.js
@@ -13,15 +13,15 @@ $slides.each(function(index){
   $(this).css('left',(index*100)+'%');
 });
 
-function startSlider() {
-  interval = setInterval(function() {
-    $slide_container.animate({'left': '-='+(width+'%')}, animation_speed, function() {
-      if (++current_slide === $slides.length) {
-        current_slide = 1;
-        $slide_container.css('left', 0);
-      }
-    });
-  }, time_val);
-}
+// function startSlider() {
+//   interval = setInterval(function() {
+//     $slide_container.animate({'left': '-='+(width+'%')}, animation_speed, function() {
+//       if (++current_slide === $slides.length) {
+//         current_slide = 1;
+//         $slide_container.css('left', 0);
+//       }
+//     });
+//   }, time_val);
+// }
 
 startSlider();
